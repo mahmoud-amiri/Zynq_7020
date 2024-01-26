2208f2aaf310ab201196a224faaccc0b814c0d33
mahmoud amiri <itsmahmoudamiri@gmail.com>
2024-01-26 16:32:28 +0100

    initial commit

---
+++ hdl/hdmi_out_wrapper.v
@@ -0,0 +1,159 @@
+`timescale 1 ps / 1 ps
+
+module hdmi_out_wrapper
+    (DDR_addr,
+    DDR_ba,
+    DDR_cas_n,
+    DDR_ck_n,
+    DDR_ck_p,
+    DDR_cke,
+    DDR_cs_n,
+    DDR_dm,
+    DDR_dq,
+    DDR_dqs_n,
+    DDR_dqs_p,
+    DDR_odt,
+    DDR_ras_n,
+    DDR_reset_n,
+    DDR_we_n,
+    FIXED_IO_ddr_vrn,
+    FIXED_IO_ddr_vrp,
+    FIXED_IO_mio,
+    FIXED_IO_ps_clk,
+    FIXED_IO_ps_porb,
+    FIXED_IO_ps_srstb,
+    iic_0_scl_io,
+    iic_0_sda_io,
+    hdmi_clk,
+    hdmi_data,
+    LCD_BL_CN,
+    LCD_PWR_EN,
+    hdmi_intn,
+    
+    hdmi_de,
+    hdmi_hs,
+    hdmi_vs);
+    
+    inout [14:0]DDR_addr;
+    inout [2:0]DDR_ba;
+    inout DDR_cas_n;
+    inout DDR_ck_n;
+    inout DDR_ck_p;
+    inout DDR_cke;
+    inout DDR_cs_n;
+    inout [3:0]DDR_dm;
+    inout [31:0]DDR_dq;
+    inout [3:0]DDR_dqs_n;
+    inout [3:0]DDR_dqs_p;
+    inout DDR_odt;
+    inout DDR_ras_n;
+    inout DDR_reset_n;
+    inout DDR_we_n;
+    inout FIXED_IO_ddr_vrn;
+    inout FIXED_IO_ddr_vrp;
+    inout [53:0]FIXED_IO_mio;
+    inout FIXED_IO_ps_clk;
+    inout FIXED_IO_ps_porb;
+    inout FIXED_IO_ps_srstb;
+    inout iic_0_scl_io;
+    inout iic_0_sda_io;
+    
+    output wire   hdmi_clk;
+    output wire[15:0] hdmi_data;
+    output wire   hdmi_de;         
+    output wire  hdmi_hs;
+    output wire   hdmi_vs;
+    input wire  [0:0]hdmi_intn;
+    
+    output  wire    LCD_BL_CN;
+    output  wire    LCD_PWR_EN;
+    
+    wire [14:0]DDR_addr;
+    wire [2:0]DDR_ba;
+    wire DDR_cas_n;
+    wire DDR_ck_n;
+    wire DDR_ck_p;
+    wire DDR_cke;
+    wire DDR_cs_n;
+    wire [3:0]DDR_dm;
+    wire [31:0]DDR_dq;
+    wire [3:0]DDR_dqs_n;
+    wire [3:0]DDR_dqs_p;
+    wire DDR_odt;
+    wire DDR_ras_n;
+    wire DDR_reset_n;
+    wire DDR_we_n;
+    wire FIXED_IO_ddr_vrn;
+    wire FIXED_IO_ddr_vrp;
+    wire [53:0]FIXED_IO_mio;
+    wire FIXED_IO_ps_clk;
+    wire FIXED_IO_ps_porb;
+    wire FIXED_IO_ps_srstb;
+    wire iic_0_scl_i;
+    wire iic_0_scl_io;
+    wire iic_0_scl_o;
+    wire iic_0_scl_t;
+    wire iic_0_sda_i;
+    wire iic_0_sda_io;
+    wire iic_0_sda_o;
+    wire iic_0_sda_t;
+    wire vid_clk;
+    wire [23:0]vid_data;
+    wire vid_de;
+    wire vid_hs;
+    wire vid_vs;
+
+    assign      LCD_BL_CN   =   1'b1;
+    assign      LCD_PWR_EN   =   1'b1;
+    assign    hdmi_data   =   {vid_data[23:19],vid_data[15:10],vid_data[7:3]};
+    assign    hdmi_clk    =   vid_clk;
+    assign    hdmi_de     =   vid_de;
+    assign    hdmi_hs     =   vid_hs;
+    assign    hdmi_vs     =   vid_vs;
+  
+  hdmi_out hdmi_out_i
+       (.DDR_addr(DDR_addr),
+        .DDR_ba(DDR_ba),
+        .DDR_cas_n(DDR_cas_n),
+        .DDR_ck_n(DDR_ck_n),
+        .DDR_ck_p(DDR_ck_p),
+        .DDR_cke(DDR_cke),
+        .DDR_cs_n(DDR_cs_n),
+        .DDR_dm(DDR_dm),
+        .DDR_dq(DDR_dq),
+        .DDR_dqs_n(DDR_dqs_n),
+        .DDR_dqs_p(DDR_dqs_p),
+        .DDR_odt(DDR_odt),
+        .DDR_ras_n(DDR_ras_n),
+        .DDR_reset_n(DDR_reset_n),
+        .DDR_we_n(DDR_we_n),
+        .FIXED_IO_ddr_vrn(FIXED_IO_ddr_vrn),
+        .FIXED_IO_ddr_vrp(FIXED_IO_ddr_vrp),
+        .FIXED_IO_mio(FIXED_IO_mio),
+        .FIXED_IO_ps_clk(FIXED_IO_ps_clk),
+        .FIXED_IO_ps_porb(FIXED_IO_ps_porb),
+        .FIXED_IO_ps_srstb(FIXED_IO_ps_srstb),
+        .IIC_0_scl_i(iic_0_scl_i),
+        .IIC_0_scl_o(iic_0_scl_o),
+        .IIC_0_scl_t(iic_0_scl_t),
+        .IIC_0_sda_i(iic_0_sda_i),
+        .IIC_0_sda_o(iic_0_sda_o),
+        .IIC_0_sda_t(iic_0_sda_t),
+        .hdmi_intn(hdmi_intn),
+        .hdmi_clk(hdmi_clk),
+        .hdmi_data(hdmi_data),
+        .hdmi_de(hdmi_de),
+        .hdmi_hs(hdmi_hs),
+        .hdmi_vs(hdmi_vs));
+
+  IOBUF iic_0_scl_iobuf
+       (.I(iic_0_scl_o),
+        .IO(iic_0_scl_io),
+        .O(iic_0_scl_i),
+        .T(iic_0_scl_t));
+  IOBUF iic_0_sda_iobuf
+       (.I(iic_0_sda_o),
+        .IO(iic_0_sda_io),
+        .O(iic_0_sda_i),
+        .T(iic_0_sda_t));
+endmodule
